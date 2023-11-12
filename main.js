883add6aecb86b232f9926a0ff6ac3c572037a8d
Mitul Das <mituldas751@gmail.com>
2023-11-12 11:00:51 +0600

    Your commit message

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings:["Automation Testing","Security Testing", "UI Testing","Manual Testing","API Testing"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000, 
+    loop: true
+});
